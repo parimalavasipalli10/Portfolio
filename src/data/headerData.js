6c865908dfbb1ab9c295be301f410ae28f2fc772
Parimala Vasipalli <59381796+parimalavasipalli10@users.noreply.github.com>
2023-06-20 18:51:17 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     desciption: "Hello! I am Parimala Vasupalli, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving.",
     image: 'https://i.ibb.co/gz7C3sZ/IMG-20230310-WA0105.jpg',
     imagebw: 'ttps://i.ibb.co/gz7C3sZ/IMG-20230310-WA0105.jpg',
-    resumePdf: 'https://drive.google.com/file/d/1V_OSaQQxkOtHM4TJz5rn85IlIyhBjk7m/view?usp=drive_link'
+    resumePdf: 'https://drive.google.com/file/d/1zq2_d4E3JRd9rc0LsXrZf4NEnkdiP0QI/view?usp=sharing'
 }
